cf7faeb0d5ec2add341c5f779be92caef49a67e0
AntDr23 <dronanton2010new@gmail.com>
2026-04-20 17:47:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,14 @@
-from flask import Flask, request, jsonify
-import logging
-import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Добавляем логирование в файл. 
-# Чтобы найти файл, перейдите на pythonwhere в раздел files, 
-# он лежит в корневой папке
-logging.basicConfig(level=logging.INFO, filename='app.log',
-                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
 
-
-@app.route('/post', methods=['POST'])
-def main():
-    logging.info('Request: %r', request.json)
-    response = {
-        'session': request.json['session'],
-        'version': request.json['version'],
-        'response': {
-            'end_session': False
-        }
-    }
-    handle_dialog(response, request.json)
-    logging.info('Request: %r', response)
-    return jsonify(response)
-
-
-def handle_dialog(res, req):
-    user_id = req['session']['user_id']
-    if req['session']['new']:  # начало диалога
-        res['response']['text'] = \
-            'Привет! Я могу показать город или сказать расстояние между городами!'
-        return
-    # TODO всю логику навыка
-
+@app.route('/')
+@app.route('/index')
+def index():
+    user = "Тестовая страница"
+    return render_template('index.html', title='Домашняя страница',
+                           username=user)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080, host='127.0.0.1')
